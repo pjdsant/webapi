b8f26d8c25e7e72aace651d372c3d70aeb5c72ab
Paulo <pjsantoss@hotmail.com>
2019-08-01 18:25:07 -0300

    Initial Source Commit

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace webapi.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public int role { get; set; }
+
+    }
+}
